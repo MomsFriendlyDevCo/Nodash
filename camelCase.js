687878b6d43e53fd78e50ae0c62194752f53597d
Matt Carter <m@ttcarter.com>
2021-03-08 11:39:52 +1000

    fix: Typo

--- camelCase.js
+++ camelCase.js
@@ -7,6 +7,6 @@
 */
 module.exports = input => input
 	.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) =>
-		index === 0 ? word.toLowerCase() : word.toUpperCase();
+		index === 0 ? word.toLowerCase() : word.toUpperCase()
 	)
 	.replace(/\s+/g, '');
